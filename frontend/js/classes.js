cfc1814e856892d715960a6699589425bed98ff3
Eduardo Antunes <edu.antunes@gmail.com>
2023-12-11 22:32:24 -0300

    Feito!

--- frontend/js/classes.js
+++
@@ -1,7 +0,0 @@
-class Anuncio {
-    constructor(id_veiculo, modelo, valor_diaria) {
-        this.id_veiculo = id_veiculo;
-        this.modelo = modelo;
-        this.valor_diaria = valor_diaria;
-    }
-}
